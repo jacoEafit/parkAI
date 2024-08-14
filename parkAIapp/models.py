830fe7bc0714a669007bdb51f97bedf7622f22fb
jacoEafit <jmontesca@eafit.edu.co>
2024-08-14 10:10:18 -0500

    update models.py

--- parkAIapp/models.py
+++ parkAIapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Vehiculo(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    placa = models.CharField(max_length=10, unique=True)
+    marca = models.CharField(max_length=50)
+    modelo = models.CharField(max_length=50)
+    color = models.CharField(max_length=30)
+
+
+class Celda(models.Model):
+    numero = models.CharField(max_length=10, unique=True)
+    disponible = models.BooleanField(default=True)
+    
+
+
+
+
+
+
+
+
