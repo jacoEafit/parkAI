f4334270ef66c05c6bd3949aafe9c7af99e28782
jacoEafit <jmontesca@eafit.edu.co>
2024-09-05 12:14:10 -0500

    Added parkingadmin template

--- parkingadmin/models.py
+++ parkingadmin/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User #Importamos modelo Usuario
+from django.contrib.auth.models import User 
 
-class Universidad(models.Model):
-    unv_id = models.AutoField(primary_key=True)
-    unv_nombre = models.CharField(max_length=40)
-    unv_direccion = models.CharField(max_length=255)
-    unv_telefono = models.CharField(max_length=20)
-    unv_correo = models.EmailField()
-    unv_logo = models.ImageField(upload_to='logos/')
+#Extensión de modelo User con rol "Organizacion" con acceso a parkingadmin e información específica
+class Organizacion(models.Model):
+    org_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True) #Id será el mismo que usuario asociado
+    org_nombre = models.CharField(max_length=40)
+    org_direccion = models.CharField(max_length=255)
+    org_telefono = models.CharField(max_length=20)
+    org_logo = models.ImageField(upload_to='logos/')
 
 
 
@@ -22,14 +22,21 @@ class Parqueadero(models.Model):
     prq_estado = models.CharField(max_length=15, choices=OPCIONES_ESTADO_PARQUEADERO, default='Activo')
     prq_precio_dia = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     prq_precio_hora = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    prq_universidad_id = models.ForeignKey(Universidad, on_delete=models.CASCADE)
+    prq_universidad_id = models.ForeignKey(Organizacion, on_delete=models.CASCADE, default=None)
+
+
+
+
+class Zona(models.Model):
+    zna_id = models.AutoField(primary_key=True)
+    zna_parqueadero_id = models.ForeignKey(Parqueadero, on_delete=models.CASCADE, default=None)
 
 
 
 
 class Conjunto_celdas(models.Model):
     cnj_id = models.AutoField(primary_key=True)
-    cnj_parqueadero_id = models.ForeignKey(Parqueadero, on_delete=models.CASCADE)
+    cnj_parqueadero_id = models.ForeignKey(Zona, on_delete=models.CASCADE,default=None)
 
 
 
@@ -42,16 +49,8 @@ class Celda(models.Model):
     
     cld_id = models.AutoField(primary_key=True)
     cld_numero_celda = models.IntegerField() #Numero de celda dentro del parqueadero
-    cld_conjunto_celdas_id = models.ForeignKey(Conjunto_celdas, on_delete=models.CASCADE)
+    cld_conjunto_celdas_id = models.ForeignKey(Conjunto_celdas, on_delete=models.CASCADE,default=None)
     cld_estado = models.CharField(max_length=20, choices=OPCIONES_ESTADO_CELDA, default='Desocupado')
-    
-
-
-
-class Carnet(models.Model):
-    crn_id = models.AutoField(primary_key=True)
-    crn_usuario_id = models.ForeignKey(User, on_delete=models.CASCADE) #Usuario al que pertenece carnet
-    crn_saldo = models.DecimalField(max_digits=10, decimal_places=2)
 
 
 
@@ -59,14 +58,14 @@ class Carnet(models.Model):
 class Vehiculo(models.Model):
     vhc_id = models.AutoField(primary_key=True)
     vhc_placa = models.CharField(max_length=15)
-    vhc_carnet_id = models.ForeignKey(Carnet, on_delete=models.CASCADE)
+    vhc_usuario_id = models.ForeignKey(User, on_delete=models.CASCADE,default=None)
 
 
 
 
 class Ingreso(models.Model):
     ing_id = models.AutoField(primary_key=True)
-    ing_vehiculo_id = models.ForeignKey(Vehiculo, on_delete=models.CASCADE)
+    ing_vehiculo_id = models.ForeignKey(Vehiculo, on_delete=models.CASCADE,default=None)
     ing_placa_vehiculo = models.CharField(max_length=15)
     ing_fecha_hora = models.DateTimeField()
 
@@ -75,7 +74,7 @@ class Ingreso(models.Model):
 
 class Egreso(models.Model):
     egr_id = models.AutoField(primary_key=True)
-    egr_vehiculo_id = models.ForeignKey(Vehiculo, on_delete=models.CASCADE)
+    egr_vehiculo_id = models.ForeignKey(Vehiculo, on_delete=models.CASCADE,default=None)
     egr_placa_vehiculo = models.CharField(max_length=15)
     egr_fecha_hora = models.DateTimeField()
 
@@ -86,6 +85,6 @@ class Factura(models.Model):
     fac_id = models.AutoField(primary_key=True)
     fac_vehiculo_id = models.ForeignKey(Vehiculo, on_delete=models.CASCADE)
     fac_placa_vehiculo = models.CharField(max_length=15)
-    fac_ingreso_id = models.DateTimeField()
-    fac_egreso_id = models.DateTimeField()
+    fac_ingreso_id = models.ForeignKey(Ingreso, on_delete=models.CASCADE, default=None)
+    fac_egreso_id = models.ForeignKey(Egreso, on_delete=models.CASCADE,default=None)
     fac_precio_neto = models.DecimalField(max_digits=10, decimal_places=2)
