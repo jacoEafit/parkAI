eb5e61677298604abfeba9f027dc3a65a693ca95
jacoEafit <jmontesca@eafit.edu.co>
2024-09-25 18:13:03 -0500

    Improvements

--- parkAIproject/settings.py
+++ parkAIproject/settings.py
@@ -115,4 +115,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/users/login'
+LOGOUT_REDIRECT_URL = '/parkingadmin'
 
--- parkingadmin/views.py
+++ parkingadmin/views.py
@@ -22,7 +22,13 @@ def ingreso_vehiculo(request):
 
 
         #Se extrae placa vehiculo con visión artificial:
-        placa_vhc_ingreso,url_imagen_recorte_placa = helpers.procesar_placa(ruta_imagen = 'media/'+nombre_imagen_vehiculo, nombre_imagen_vehiculo = imagen_vehiculo.name)
+        resultados_procesamiento = helpers.procesar_placa(ruta_imagen = 'media/'+nombre_imagen_vehiculo, nombre_imagen_vehiculo = imagen_vehiculo.name)
+        
+        #Se valida si se obtuvo la placa correctamente
+        if isinstance(resultados_procesamiento,tuple):#Si se detecto una placa
+            placa_vhc_ingreso,url_imagen_recorte_placa = resultados_procesamiento
+        else:
+            placa_vhc_ingreso = resultados_procesamiento
 
         #Se valida si dicho vehículo existe:
         try:
@@ -30,9 +36,6 @@ def ingreso_vehiculo(request):
             vehiculo_existe = True
         except ObjectDoesNotExist:
             vehiculo_existe = False
-            #Se crea vehiculo, pero luego mandar a otro lugar de crear vehiculo y usuario
-            #vehiculo = Vehiculo(vhc_placa = placa_vhc_ingreso, vhc_usuario_id = 1)
-            #vehiculo.save()
         
         #Se crea el ingreso:
         #ingreso = Ingreso(ing_vehiculo_id = vehiculo.vhc_id, ing_placa_vehiculo = vehiculo.vhc_placa, ing_fecha_hora=timezone.now(), ing_imagen_vehiculo = 'imagenes_vehiculos/'+nombre_imagen_vehiculo)
@@ -45,4 +48,6 @@ def ingreso_vehiculo(request):
     #Si no han ingresado imagen
     elif request.method == 'GET':
         return render(request,'ingreso_vehiculo.html')
+
+
 
